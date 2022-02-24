6510279bbc4a4514fa1e3279c281fd1740af2916
Lightii Chen <horizones.studio@gmail.com>
2022-02-25 03:28:39 +0800

    a more finished draft

--- App.js
+++ App.js
@@ -1,27 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import {Image, StyleSheet, Text, View} from 'react-native';
 import {ScrollView} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 import {Card} from "./src/components/Card";
 
 export default function App() {
-  return (
-     <SafeAreaView>
-         <ScrollView style={{
-            marginTop: 32,
-            marginHorizontal: 16,
-            marginBottom: 16
-         }}>
-            <View
-               style={{
-                  height: 320, width: "100%", alignItems: "center"
-            }}>
-               <Image style={{ height: 240, width: 240, borderRadius: 16 }} source={{ uri: "https://lh3.googleusercontent.com/iY1TONzU7GOInHvPotAnwx6viU2Wg2qS8Uzvd7EyPn4E7id34k8DIX6oh0BNgjgg7rAslW_cJSMQpnWomQ=w544-h544-l90-rj" }}/>
+	return (
 
-            </View>
-            <Card uri="https://i.imgur.com/wcl21SV.png" title="HI" description="its so hilarious"/>
-         </ScrollView>
-     </SafeAreaView>
+		<SafeAreaView>
 
-  );
+			<View style={{
+				// flex:1,
+				height: "100%",
+				marginTop: 32,
+				paddingBottom: 64,
+				// backgroundColor: "lightgray",
+				marginHorizontal: 24
+			}}>
+
+				<View
+					style={{
+						height: 320, width: "100%", alignItems: "center"
+					}}>
+					<View style={{
+						height: 240,
+						width: 240,
+						borderRadius: 16,
+						elevation: 16
+					}}>
+						<Image style={{
+							height: 240,
+							width: 240,
+							borderRadius: 16,
+						}}
+						       source={{uri: "https://lh3.googleusercontent.com/iY1TONzU7GOInHvPotAnwx6viU2Wg2qS8Uzvd7EyPn4E7id34k8DIX6oh0BNgjgg7rAslW_cJSMQpnWomQ=w544-h544-l90-rj"}}
+						/>
+					</View>
+
+				</View>
+
+				<ScrollView style={{
+					height: 0,
+				}}>
+					<Card
+						uri="https://lh3.googleusercontent.com/iY1TONzU7GOInHvPotAnwx6viU2Wg2qS8Uzvd7EyPn4E7id34k8DIX6oh0BNgjgg7rAslW_cJSMQpnWomQ=w544-h544-l90-rj"
+						title="Intro: Lost" artist="Kim Wooseok"/>
+					<View style={{marginLeft: 52, height: 1, width: "100%", backgroundColor: "lightgray"}}/>
+
+					<Card
+						uri="https://lh3.googleusercontent.com/iY1TONzU7GOInHvPotAnwx6viU2Wg2qS8Uzvd7EyPn4E7id34k8DIX6oh0BNgjgg7rAslW_cJSMQpnWomQ=w544-h544-l90-rj"
+						title="Red Moon" artist="Kim Wooseok"/>
+					<View style={{marginLeft: 52, height: 1, width: "100%", backgroundColor: "lightgray"}}/>
+
+					<Card
+						uri="https://lh3.googleusercontent.com/iY1TONzU7GOInHvPotAnwx6viU2Wg2qS8Uzvd7EyPn4E7id34k8DIX6oh0BNgjgg7rAslW_cJSMQpnWomQ=w544-h544-l90-rj"
+						title="SINPhony" artist="Kim Wooseok"/>
+					<View style={{marginLeft: 52, height: 1, width: "100%", backgroundColor: "lightgray"}}/>
+
+					<Card
+						uri="https://lh3.googleusercontent.com/iY1TONzU7GOInHvPotAnwx6viU2Wg2qS8Uzvd7EyPn4E7id34k8DIX6oh0BNgjgg7rAslW_cJSMQpnWomQ=w544-h544-l90-rj"
+						title="Somebody Like You" artist="Kim Wooseok"/>
+					<View style={{marginLeft: 52, height: 1, width: "100%", backgroundColor: "lightgray"}}/>
+
+					<Card
+						uri="https://lh3.googleusercontent.com/iY1TONzU7GOInHvPotAnwx6viU2Wg2qS8Uzvd7EyPn4E7id34k8DIX6oh0BNgjgg7rAslW_cJSMQpnWomQ=w544-h544-l90-rj"
+						title="Do U Like" artist="Kim Wooseok"/>
+					<View style={{marginLeft: 52, height: 1, width: "100%", backgroundColor: "lightgray"}}/>
+
+					<Card
+						uri="https://lh3.googleusercontent.com/iY1TONzU7GOInHvPotAnwx6viU2Wg2qS8Uzvd7EyPn4E7id34k8DIX6oh0BNgjgg7rAslW_cJSMQpnWomQ=w544-h544-l90-rj"
+						title="Beautiful" artist="Kim Wooseok"/>
+					<View style={{marginLeft: 52, height: 1, width: "100%", backgroundColor: "lightgray"}}/>
+
+					<Card
+						uri="https://lh3.googleusercontent.com/iY1TONzU7GOInHvPotAnwx6viU2Wg2qS8Uzvd7EyPn4E7id34k8DIX6oh0BNgjgg7rAslW_cJSMQpnWomQ=w544-h544-l90-rj"
+						title="The Winter" artist="Kim Wooseok"/>
+					<View style={{marginLeft: 52, height: 1, width: "100%", backgroundColor: "lightgray"}}/>
+
+				</ScrollView>
+
+			</View>
+
+		</SafeAreaView>
+
+	);
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,44 @@
-// import React from "react"
-
 import {Text, View} from "react-native";
 import {Image} from "react-native";
 
 export const Card = (props) =>{
 	return(
 		<View style={{
-			height: 120,
+			height: 64,
 			width: "100%",
-			flexDirection:　"row"
+			flexDirection:　"row",
+			// backgroundColor: "black"
 		}}>
-				<Text>{props.title}</Text>
-				<Text>{props.description}</Text>
+
+			<View style={{
+				height: 64,
+				justifyContent: "center"
+			}}>
+				<Image style={{
+					height: 42,
+					width: 42,
+					borderRadius: 6,
+				}} source={{ uri: props.uri}}/>
+			</View>
+
+
+			<View style={{
+				marginLeft: 6,
+				padding: 8,
+				height: 64,
+				width: "100%",
+				flexDirection:　"column",
+				justifyContent: "space-around"
+			}}>
+				<Text style={{
+					fontWeight: "700",
+					fontSize: 16
+				}}>{props.title}</Text>
+				<Text style={{
+					color: "gray"
+				}}>{props.artist}</Text>
+			</View>
+
 		</View>
 	)
 }
